9106c612360b16dbd9c3e6ac45bc9e5683eb9dd1
carlos-brazon-ma <148769457+carlos-brazon-ma@users.noreply.github.com>
2023-11-08 00:26:49 +0100

    arreglado tamaño boton urgente

--- src/components/MarketList.jsx
+++ src/components/MarketList.jsx
@@ -141,11 +141,11 @@ const MarketList = () => {
           list?.map((item, index) => {
             if (item.tags === button) {
               return <li
-                className={`list-disc list-inside break-normal items-center justify-between flex gap-4 rounded py-0.5 px-2 ${item.isDone ? 'line-through' : ''} ${item.priority ? 'bg-red-300' : index % 2 === 0 ? 'bg-blue-200' : 'bg-slate-50'}`}
+                className={`list-disc list-inside break-normal items-center justify-between flex gap-4 rounded py-1 px-2 ${item.isDone ? 'line-through' : ''} ${item.priority ? 'bg-red-300' : index % 2 === 0 ? 'bg-blue-200' : 'bg-slate-50'}`}
                 key={index}
               >
                 <div className='w-full text-lg' onClick={() => handleClick(item)}>{firstLetterUpperCase(item.name)}</div>
-                <div onClick={() => handlePriority(item)} className={`flex items-center w-auto h-6 z-50 rounded text-xs text-center px-0.5 ${priority ? 'bg-red-400' : 'bg-slate-400'}`}>Urgente</div>
+                <div onClick={() => handlePriority(item)} className={`flex items-center w-auto h-7 z-50 rounded text-xs text-center px-0.5 ${priority ? 'bg-red-400' : 'bg-slate-400'}`}>Urgente</div>
               </li>
             }
           })
