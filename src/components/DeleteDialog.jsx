a8b0d4c32e44238dd5e509df8d0fc38733bbb8f6
carlos-brazon-ma <148769457+carlos-brazon-ma@users.noreply.github.com>
2024-04-09 00:44:40 +0200

    Update DeleteDialog.jsx

--- src/components/DeleteDialog.jsx
+++ src/components/DeleteDialog.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from './ui/alert-dialog';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db2 } from '../utils/firebase';
@@ -8,6 +8,7 @@ import { Button } from './ui/button';
 
 const DeleteDialog = () => {
     const { button, setButton, setList, userIn, setAddTags, setSelectedTag } = useContext(AllItemsContext);
+    const [done, setDone] = useState(false)
 
     const handleClick = async () => {
         setAddTags(false);
@@ -35,23 +36,50 @@ const DeleteDialog = () => {
             await updateDoc(userDocRef, { last_tags: tags[0] || '', markeList: updatedMarkeList });
         }
     }
+    const handleDeleteDone = async () => {
+        setAddTags(false);
+        const userDocRef = doc(db2, 'usersMarketList', userIn.uid);
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        if (userDocSnapshot.exists()) {
+            const userDocData = userDocSnapshot.data();
+            const updatedMarkeList = userDocData.markeList.filter(item => item.isDone == false);
+            const tags = updatedMarkeList.reduce((acc, obj) => {
+                if (obj.tags) {
+                    if (!acc.includes(obj.tags)) {
+                        acc.push(obj.tags);
+                    }
+                }
+                return acc
+            }, []);
+            if (tags.length == 1) {
+                setButton(tags[0])
+            }
+            setList(updatedMarkeList);
+            setSelectedTag(updatedMarkeList);
+            await updateDoc(userDocRef, { last_tags: tags.length > 1 && tags.includes(button) ? button : tags[0] || '', markeList: updatedMarkeList });
+        }
+
+    }
 
     return (
         <AlertDialog>
             <AlertDialogTrigger asChild>
-                <Button className="">Eliminar lista</Button>
-                {/* <Button className="">Elimina</Button> */}
+                <div className='flex gap-2'>
+                    <Button onClick={() => setDone(true)} className="">Eliminar lista</Button>
+                    <Button onClick={() => setDone(false)} className="">Elimina tachados</Button>
+                </div>
             </AlertDialogTrigger>
             <AlertDialogContent>
                 <AlertDialogHeader>
-                    <AlertDialogTitle>¿Deseas borrar la lista? {firstLetterUpperCase(button ?? '')}</AlertDialogTitle>
+                    <AlertDialogTitle>{done ? '¿Deseas borrar la lista' : "¿Deseas borrar todos los tachados de la lista"} {firstLetterUpperCase(button + '?' ?? '')}</AlertDialogTitle>
                     <AlertDialogDescription>
                         Esta acción no se puede deshacer. Esto eliminará permanentemente la lista.
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                     <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction onClick={() => handleClick()}>Continue</AlertDialogAction>
+                    <AlertDialogAction onClick={() => done ? handleClick() : handleDeleteDone()}>Continue</AlertDialogAction>
                 </AlertDialogFooter>
             </AlertDialogContent>
         </AlertDialog>
