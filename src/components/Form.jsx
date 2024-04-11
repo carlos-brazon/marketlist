1079816f1f8225b161a4c4c8326c136a54d159df
Carlos_Brazom <129105444+carlos-brazon@users.noreply.github.com>
2024-04-11 11:31:44 +0200

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -41,7 +41,7 @@ const Form = () => {
             const listFilterTags = marketListUser.filter(item => item.tags === button)
             const productExists = listFilterTags.some(item => item.tags === user.tags && item.name === user.name.trim());
             if (!productExists) {
-                const date =Timestamp.fromDate( new Date())
+                const date = new Date()
                 setUser(prev => ({ ...prev, name: '' }));
                 setAddTags(false)
                 toast({
@@ -49,7 +49,8 @@ const Form = () => {
                     duration: '1000',
                 })
                 setButton(user.tags.trim());
-                setList(prev => [...prev, { ...user, isDone: false, priority: false, id: newId, name: user.name.toLowerCase(), tags: user.tags.trim(), create_at:date }]);
+                const timestamp = Timestamp.fromDate(date);
+                setList(prev => [...prev, { ...user, isDone: false, priority: false, id: newId, name: user.name.toLowerCase(), tags: user.tags.trim(), create_at:timestamp }]);
                 setSelectedTag(prev => [...prev, { ...user, isDone: false, priority: false, id: newId, name: user.name.toLowerCase(), tags: user.tags.trim() }])
                 const newId = doc(collection(db2, 'newId')).id;
                 await updateDoc(doc(db2, 'usersMarketList', userIn.uid), {
