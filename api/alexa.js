9f1b32d5400d42e0516b6149e9f039d70b1c1d10
Carlos_Brazom <129105444+carlos-brazon@users.noreply.github.com>
2025-09-18 05:45:35 +0200

    update api

--- api/alexa.js
+++ api/alexa.js
@@ -1,32 +1,3 @@
-// import admin from "firebase-admin";
-
-// if (!admin.apps.length) {
-//   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   });
-// }
-
-// const db = admin.firestore();
-
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") return res.status(405).send("Solo POST permitido");
-
-//   const item = req.body.queryResult?.parameters?.item;
-//   if (!item) return res.status(400).json({ error: "No se recibió item" });
-
-//   await db.collection("dataItemsMarketList2").add({
-//     name: item.toLowerCase(),
-//     tags: "compras",
-//     isDone: false,
-//     create_at: new Date(),
-//     amount: 0,
-//   });
-
-//   res.status(200).json({ fulfillmentText: `¡Agregué ${item} a tu lista!` });
-// }
-// /////// hasta aqui funciona con postman 18-09-25 04:19h
-
 import admin from "firebase-admin";
 
 // Inicializar Firebase Admin
@@ -57,7 +28,7 @@ export default async function handler(req, res) {
     item = Array.isArray(item) ? item[0] : item;
 
     // Guardar en Firestore
-    const docRef = db.collection("dataItemsMarketList2").doc(); // genera un ID
+    const docRef = db.collection("dataItemsMarketList").doc(); // genera un ID
     const docId = docRef.id;
 
     await docRef.set({
