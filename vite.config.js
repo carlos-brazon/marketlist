d73965981979418483a3ecde444ac7ffe07d0752
carlos-brazon-ma <brazoncm03@gmail.com>
2025-09-29 22:21:01 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,11 @@
 
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+// import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
