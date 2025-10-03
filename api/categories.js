589c0d45966f19ab0dffa67b6ff041ff26fddcbd
carlos-brazon-ma <brazoncm03@gmail.com>
2025-10-03 18:50:50 +0200

    update

--- api/categories.js
+++ api/categories.js
@@ -1,3 +1,69 @@
+// import fetch from "node-fetch";
+
+// export default async function handler(req, res) {
+//   try {
+//     // 1️⃣ Obtener todas las categorías principales
+//     const response = await fetch(
+//       "https://tienda.mercadona.es/api/categories/",
+//       {
+//         headers: {
+//           "User-Agent":
+//             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+//           Accept: "application/json",
+//         },
+//       }
+//     );
+
+//     if (!response.ok) {
+//       return res
+//         .status(response.status)
+//         .json({ error: "Error al obtener categorías principales" });
+//     }
+
+//     const data = await response.json();
+
+//     // Extraer todos los IDs de subcategorías
+//     const ids = data.results.flatMap((cat) =>
+//       cat.categories.map((sub) => sub.id)
+//     );
+
+//     // const testId = ['112','161']
+
+//     // 2️⃣ Obtener datos de cada subcategoría con Promise.allSettled
+//     try {
+//       const allDataResults = await Promise.allSettled(
+//         ids.map(
+//           async (id) =>
+//             await fetch(`https://tienda.mercadona.es/api/categories/${id}/`, {
+//               headers: {
+//                 "User-Agent":
+//                   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+//                 Accept: "application/json",
+//               },
+//             }).then((res) => {
+//               if (!res.ok) throw new Error(`Error al obtener categoría ${id}`);
+//               return res.json();
+//             })
+//         )
+//       );
+//       // Filtrar las respuestas exitosas
+//       const allData = allDataResults
+//         .filter((r) => r.status === "fulfilled")
+//         .map((r) => r.value);
+
+//       // 3️⃣ Enviar la respuesta final
+//       res.status(200).json({
+//         subcategories: allData,
+//       });
+//     } catch (error) {
+//       console.error("Error general en handler ids:", error);
+//       res.status(500).json({ error: "Error en el servidor ids" });
+//     }
+//   } catch (err) {
+//     console.error("Error general en handler:", err);
+//     res.status(500).json({ error: "Error en el servidor" });
+//   }
+// }
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
