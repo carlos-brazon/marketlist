7b94e40f61aeb007e5f8f5ad408ddb17ee89d81f
carlos-brazon-ma <brazoncm03@gmail.com>
2025-10-11 22:03:50 +0200

    add axio for chunks

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,34 @@
-// // import { defineConfig } from "vite";
-// // import react from "@vitejs/plugin-react";
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
 
-// // // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // });
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import tailwindcss from '@tailwindcss/vite' 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: ["axios", "lodash"], // agrega librerías que uses mucho
+        },
+      },
     },
   },
 });
